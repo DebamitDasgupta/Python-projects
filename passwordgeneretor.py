32e775a9cb5f0aeb5356b4b65aa53996b24b5b0a
DebamitDasgupta <95354270+DebamitDasgupta@users.noreply.github.com>
2021-12-01 04:23:21 -0800

    make a password genretor uesing pyhton

---
+++ passwordgeneretor.py
@@ -0,0 +1,11 @@
+import random
+
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+number = '1234567890'
+symbol = '!@#$%^&*()_+<>?":|'
+
+all=lower + upper + number + symbol
+length = 9
+password="".join(random.sample(all,length))
+print("YOUR PASSWORD IS :",password)
